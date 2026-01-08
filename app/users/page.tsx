ef5c1cc31da0f528bb99f6875a70d5511762a156
San tol <tolsan42@gmail.com>
2026-01-09 03:18:54 +0700

    Fetch data

--- app/users/page.tsx
+++ app/users/page.tsx
@@ -1,8 +1,19 @@
-import React from 'react'
+interface User {
+  id: number;
+  name: string; 
+}
 
-const UserPage = () => {
+const UserPage = async () => {
+
+  const res = await fetch("https://jsonplaceholder.typicode.com/users"); // Updated endpoint
+  const users: User[] = await res.json();
+
   return (
-    <div>UserPage</div>
+    <div>
+      <ul>
+        {users.map((user) => (<li key={user.id}>{user.name}</li>))}
+      </ul>
+    </div>
   )
 }
 
