30747473dc99b46c947547221e2cfb23ca0038cc
San tol <tolsan42@gmail.com>
2026-01-09 11:33:17 +0700

    Static vs. Dynamic Rendering

--- app/users/page.tsx
+++ app/users/page.tsx
@@ -5,11 +5,13 @@ interface User {
 
 const UserPage = async () => {
 
-  const res = await fetch("https://jsonplaceholder.typicode.com/users"); // Updated endpoint
+  const res = await fetch("https://jsonplaceholder.typicode.com/users", {cache: 'no-cache'}); 
   const users: User[] = await res.json();
 
   return (
     <div>
+      <h1>User</h1>
+      <p>{new Date().toLocaleTimeString()}</p>
       <ul>
         {users.map((user) => (<li key={user.id}>{user.name}</li>))}
       </ul>
