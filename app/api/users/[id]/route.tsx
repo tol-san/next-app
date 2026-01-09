86220e52429e859a99ec1de33a8dc5f279a5bc70
San tol <tolsan42@gmail.com>
2026-01-09 23:58:38 +0700

    Update an object

--- app/api/users/[id]/route.tsx
+++ app/api/users/[id]/route.tsx
@@ -18,3 +18,14 @@ export async function GET(request: NextRequest, { params }: Props) {
     )
 }
 
+
+export async function PUT(request: NextRequest, { params }: Props) {
+    const { id } = await params;
+    const body = await request.json();
+
+    if (!body.name)
+        return NextResponse.json({ error: "Name is required" }, { status: 400 })
+    if (id > 10)
+        return NextResponse.json({ error: "User not found" }, { status: 404 })
+    return NextResponse.json({ id: 1, name: body.name })
+}
