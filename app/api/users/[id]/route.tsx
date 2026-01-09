2b03286f2cac445e32a14801e15de3fc1bbfba98
San tol <tolsan42@gmail.com>
2026-01-10 00:04:56 +0700

    Delete object

--- app/api/users/[id]/route.tsx
+++ app/api/users/[id]/route.tsx
@@ -29,3 +29,12 @@ export async function PUT(request: NextRequest, { params }: Props) {
         return NextResponse.json({ error: "User not found" }, { status: 404 })
     return NextResponse.json({ id: 1, name: body.name })
 }
+
+
+export async function DELETE(request: NextRequest, { params }: Props) {
+    const { id } = await params;
+    if (id > 10)
+        return NextResponse.json({ error: "User not found" }, { status: 404 })
+    return NextResponse.json({})
+}
+
